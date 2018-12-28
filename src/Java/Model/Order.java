a56c608f2f6ac38d358ddbd4df0bc54312d35974
"Khanh Dang" <"dangnhatkhanh97@gmail.com">
2018-12-28 17:51:35 +0700

    Added: JSON library

---
+++ src/Java/Model/Order.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Khanh
+ */
+public class Order {
+    int id;
+    String filename;
+    String username;
+    String description;
+    String status;
+
+    public Order(int id, String filename, String username, String description, String status) {
+        this.id = id;
+        this.filename = filename;
+        this.username = username;
+        this.description = description;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
